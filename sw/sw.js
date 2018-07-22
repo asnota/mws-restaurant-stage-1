ca60649f8ef9f1e51f8be02b4a2be3ec531a11c9
Anna Shvets <annashvets11@gmail.com>
2018-07-22 20:06:09 +0200

    Extended sw.js functionality

--- sw/sw.js
+++ sw/sw.js
@@ -1,3 +1,6 @@
+//var r=FetchEvent.prototype.respondWith
+//FetchEvent.prototype.respondWith=function(){return new URL(this.request.url).search.endsWith("bypass-sw")?void 0:r.apply(this,arguments)}
+
 let staticCacheName = 'restaurant-static-v1';
 
 self.addEventListener('install', function(event) {
@@ -5,14 +8,43 @@ self.addEventListener('install', function(event) {
         caches.open(staticCacheName).then(function(cache) {
             return cache.addAll([
                 '/',
-                'css/styles.css',
-                'data/restaurants.json',
-                'img/',
-                'js/dbhelper.js',
-                'js/main.js',
-                'js/restaurant_info.js',
-                'sw/sw.js'
+                '/css/styles.css',
+                '/data/restaurants.json',
+                '/img/',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/sw/sw.js'
             ]);
         })
     );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') && cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) return response;
+            return fetch(event.request)
+        })
+    );
+});
+
+self.addEventListener('message', function(event) {
+    if (event.data.action === 'skipWaiting') {
+       self.skipWaiting();
+    }
 });
